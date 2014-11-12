e5415089436a2b4548ebbe9528eb5019c6cf8254
oraclemagic <magic@jehy.ru>
2014-11-12 13:07:38 +0400

    correct settings file

--- scripts/settings.sample.php
+++ scripts/settings.sample.php
@@ -6,7 +6,7 @@ $CURR_SESSION='DEFAULT';
 $CONNECT_POOL=array();
 $normal_errors=array(24344,955,1917,942);#errors to ignore
 $normal_errors[]=20102;
-$CONNECTS=array('DEFAULT'=>array('scheme'=>'magic','pass'=>'423826','connect'=>'//192.168.120.142:1521/devel.krk.sma.ora','enc'=>'CL8MSWIN1251','mode'=>OCI_DEFAULT),);#your oracle connection settings
+$CONNECTS=array('DEFAULT'=>array('scheme'=>'magic','pass'=>'YOUR_PASSWORD','connect'=>'//host:1521/sid','enc'=>'AL32UTF8','mode'=>OCI_DEFAULT),);#your oracle connection settings
 define('VERBOSE',0);
 define('DEBUG',1);
 $bkp_dir = '/repo/default.git/';#define a directory to store your git repo
